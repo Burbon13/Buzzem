8c020feed6ad1998955ec31b54981da427934be5
Burbon13 <razvan_roatis@yahoo.com>
2018-08-04 21:20:04 +0300

    Main menu and Buzz menu added
    (-_-)

--- app/src/main/java/com/burbon13/buzzem/BuzzActivity.kt
+++ app/src/main/java/com/burbon13/buzzem/BuzzActivity.kt
@@ -3,20 +3,17 @@ package com.burbon13.buzzem
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
-import android.view.Window
-import android.view.WindowManager
 
-class MainActivity : AppCompatActivity() {
+class BuzzActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_buzz)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
-        inflater.inflate(R.menu.main_menu,menu)
+        inflater.inflate(R.menu.buzz_menu,menu)
         return super.onCreateOptionsMenu(menu)
     }
 }
